5b70f18c8042d7c7bf43f79483f2a74ffe351ae4
SofiaHritcu <sofia.hritcu@principal33.com>
2021-08-23 13:57:18 +0300

    #template_porfolio - add: login view and login components folder added

---
+++ src/views/Login.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <v-container class="grey lighten-5 mb-6 my-5">
+            <v-row no-gutters> 
+                Welcome to our app !
+                <v-col cols="5">
+
+                </v-col>
+                <v-col cols="2">
+
+                </v-col>
+                <v-col cols="5">
+
+                </v-col>
+            </v-row>
+        </v-container>
+    </div>
+
+</template>
+
+<script>
+export default {
+    name: 'Login',
+
+    components: {
+    },
+}
+</script>
+
+<style>
+
+</style>
