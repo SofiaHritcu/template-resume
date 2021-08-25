c8603f935b31dd90937a3a83fa9ee3b23d0cd1be
jesq <57216949+jesq@users.noreply.github.com>
2021-08-25 11:25:19 +0300

    Feature/#37 edit portfolio (#10)
    * #template-portfolio - add: navbar edited for logged in user and added edit path
    
    * Added: fetch current user data into the navbar
    
    * #template-portfolio - update: private routes and navbar update
    
    * Edited: font scaling for user name on navbar
    
    * #template-portfolio - update: when landing on a page in sidebar the page gets different style
    
    * Added: display profile image from firebase storage
    
    * #template-portfolio - update: navbar with setting button
    
    * Fixed: always rounded profile picture inside navbar
    
    * #template-portfolio - add: settings button on navbar and settings view
    
    * Updated: now social media icons direct to each individual profile
    
    * #template-portfolio - add: settings form for editting user profile
    
    * #template-porrtfolio - add: upload piture on settings
    
    Co-authored-by: SofiaHritcu <61913547+SofiaHritcu@users.noreply.github.com>

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,17 +1,19 @@
 <template>
   <body>
     <div class="sidebar">
-      <h1 class="text-h3 font-weight-thin ">{{ name }}</h1>
-      <img v-bind:src="image_src" class="imgg" />
+      <h1 class="text-h5 font-weight-thin" v-if="isLongName"> {{ name }} </h1>
+      <h1 class="text-h3 font-weight-thin" v-else> {{ name }} </h1>
+      <v-avatar size="250">
+        <v-img v-bind:src="profileImage"
+          max-height="200"
+          max-width="200">
+        </v-img>
+      </v-avatar>
       <p class="description text-h6 font-weight-thin">{{ description }}</p>
       <div class="socials">
-        <a href="https://www.linkedin.com/" class="fa fa-linkedin d-inline"></a>
-        <a href="https://www.github.com/" class="fa fa-github d-inline"></a>
-        <a
-          href="https://www.instagram.com/"
-          class="fa fa-instagram d-inline"
-        ></a>
-        <a href="https://www.yahoo.com/" class="fa fa-yahoo d-inline"></a>
+        <a :href="`${ linkedInLink }`" class="fa fa-linkedin d-inline" v-if="linkedInLink"></a>
+        <a :href="`${ gitHubLink }`" class="fa fa-github d-inline" v-if="gitHubLink"></a>
+        <a :href="`${ instagramLink }`" class="fa fa-instagram d-inline" v-if="instagramLink"></a>
       </div>
       <hr />
       <div class="sections">
@@ -31,7 +33,7 @@
               d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"
             />
           </svg>
-          <a href="#" class="section-link text-heading-5 font-weight-light my-2"
+          <a href="/home" :class="[onAbout ? 'activeClass' : '','section-link', 'text-heading-5', 'font-weight-light', 'my-2']"
             >About Me</a
           >
         </div>
@@ -52,7 +54,7 @@
               d="M1 6v-.5a.5.5 0 0 1 1 0V6h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1H1zm0 3v-.5a.5.5 0 0 1 1 0V9h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1H1zm0 2.5v.5H.5a.5.5 0 0 0 0 1h2a.5.5 0 0 0 0-1H2v-.5a.5.5 0 0 0-1 0z"
             />
           </svg>
-          <a href="#" class="section-link text-heading-5 font-weight-light my-2"
+          <a href="/portfolio" :class="[onPortfolio ? 'activeClass' : '','section-link', 'text-heading-5', 'font-weight-light', 'my-2']"
             >Portfolio</a
           >
         </div>
@@ -73,7 +75,7 @@
               d="M5 8a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7A.5.5 0 0 1 5 8zm0-2.5a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5zm0 5a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5zm-1-5a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0zM4 8a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0zm0 2.5a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0z"
             />
           </svg>
-          <a href="#" class="section-link text-heading-5 font-weight-light my-2"
+          <a href="/resume" :class="[onResume ? 'activeClass' : '','section-link', 'text-heading-5', 'font-weight-light', 'my-2']"
             >Resume</a
           >
         </div>
@@ -94,28 +96,145 @@
               d="m2.165 15.803.02-.004c1.83-.363 2.948-.842 3.468-1.105A9.06 9.06 0 0 0 8 15c4.418 0 8-3.134 8-7s-3.582-7-8-7-8 3.134-8 7c0 1.76.743 3.37 1.97 4.6a10.437 10.437 0 0 1-.524 2.318l-.003.011a10.722 10.722 0 0 1-.244.637c-.079.186.074.394.273.362a21.673 21.673 0 0 0 .693-.125zm.8-3.108a1 1 0 0 0-.287-.801C1.618 10.83 1 9.468 1 8c0-3.192 3.004-6 7-6s7 2.808 7 6c0 3.193-3.004 6-7 6a8.06 8.06 0 0 1-2.088-.272 1 1 0 0 0-.711.074c-.387.196-1.24.57-2.634.893a10.97 10.97 0 0 0 .398-2z"
             />
           </svg>
-          <a href="#" class="section-link text-heading-5 font-weight-light my-2"
+          <a href="/contact" :class="[onContact ? 'activeClass' : '','section-link', 'text-heading-5', 'font-weight-light', 'my-2']"
             >Contact</a
           >
         </div>
       </div>
+      <hr />
+      <v-row class="sections" justify="center" align="center" :v-if="isLoggedIn">
+        <div class="my-3 mr-15 d-flex">
+          <v-btn
+              large
+              color="#85a3e0"
+              :class="[onEditPortfolio ? 'activeButton' : '']"
+              class="white--text classic-button"
+              @click="editPortfolio"
+          >
+              <v-icon left color="white">
+                  mdi-account-edit-outline
+              </v-icon>
+              Edit my portfolio
+          </v-btn>
+        </div>
+        <div class="my-3 ml-6 d-flex">
+          <v-btn
+              large
+              color="#85a3e0"
+              :class="[onEditProfile ? 'activeButton' : '']"
+              class="white--text classic-button"
+              @click="settings"
+          >
+              <v-icon left color="white">
+                  mdi-cogs
+              </v-icon>
+              Settings
+          </v-btn>
+        </div>
+        <div class="my-3 ml-8 d-flex">
+          <v-btn
+              large
+              color="#85a3e0"
+              class="white--text classic-button"
+              @click="logOut"
+          >
+              <v-icon left color="white">
+                  mdi-account-arrow-right-outline
+              </v-icon>
+              Log out
+          </v-btn>
+        </div>
+      </v-row>
     </div>
   </body>
 </template>
 
 <script>
 // :href="name"
-import image from "../assets/testImg.jpg";
+// import image from "../assets/testImg.jpg";
+import image from "../assets/account.png";
+import firebase from 'firebase'
+import 'firebase/storage'
+import db from '@/firebase/firebaseInit.js'
+
 export default {
-  props: ["name"],
+  props: ['onAbout', 'onPortfolio', 'onResume', 'onContact', 'onEditPortfolio', 'onEditProfile'],
   data() {
     return {
-      name: "Some Dude",
-      image_src: image,
+      userID:'',
+      firstName: 'some',
+      lastName:'dude',
+      isLongName:'',
+      name: '',
+      profileImage: image,
       description:
         "Hi, my name is Some Dude and I'm a senior software engineer. Welcome to my personal website!",
+      linkedInLink: '',
+      gitHubLink: '',
+      instagramLink: '',
     };
   },
+  computed: {
+    isLoggedIn() {
+      firebase.auth().onAuthStateChanged(user => {
+        if (!user) {
+          return false;
+        } else {
+          this.userID = user.uid;
+          console.log(this.userID);
+          this.fetchCurrentUserData();
+          return true;
+        }
+    });
+    },
+  },
+  methods: {
+    longName() {
+      console.log(this.name);
+      console.log(this.name.length > 12);
+      if(this.name.length > 12){
+        this.isLongName = true;
+      }
+      else {
+        this.isLongName = false;
+      }
+    },
+    editPortfolio() {
+      this.$router.push('/edit')
+    },
+    settings() {
+      this.$router.push('/settings')
+    },
+    logOut() {
+      console.log("logging out");
+    },
+    fetchCurrentUserData() {
+      db.collection('users').doc(this.userID).get()
+      .then((doc) => {
+        if (doc.exists) {
+          this.firstName = doc.data().firstName;
+          this.lastName = doc.data().lastName;
+          this.name = this.firstName + ' ' + this.lastName;
+          this.longName();
+          this.description = doc.data().description;
+          this.linkedInLink = doc.data().linkedInLink;
+          this.instagramLink = doc.data().instagramLink;
+          this.gitHubLink = doc.data().gitHubLink;
+          var storageRef = firebase.storage().ref(`profilePictures/${this.userID}.jpg`).getDownloadURL()
+          .then((url)=> {
+            this.profileImage = url;
+          })
+        }
+        else {
+          console.log("No such document!");
+          this.$router.push('/login');
+        }
+      }).catch((error) => {
+        console.log("Error getting document: ", error)
+        this.$router.push('/login');
+      })
+    }
+  }
 };
 </script>
 
@@ -137,12 +256,6 @@ export default {
 h1 {
   color: #adc2eb;
   font-size: 32px;
-}
-.imgg {
-  width: 170px;
-  height: auto;
-  border-radius: 50%;
-  margin-top: 20px;
 }
 .description {
   padding-top: 15px;
@@ -176,6 +289,20 @@ hr {
 
 .linkss:hover {
   opacity: 0.7;
+}
+
+.classic-button:hover{
+    background-color:#3366cc !important;
+}
+
+.activeClass {
+    color:#85a3e0 !important;
+    font-weight: 400 !important;
+}
+
+.activeButton, .activeButton:hover {
+  color: #3366cc !important;
+  background-color:#85a3e0 !important;
 }
 
 @media only screen and (max-width: 768px) {
--- src/router/index.js
+++ src/router/index.js
@@ -1,20 +1,37 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Edit from '../views/Edit.vue'
+import Settings from '../views/Settings.vue'
 import Login from '../views/Login.vue'
 import SignUp from '../views/SignUp.vue'
 import ForgotPassword from '../views/ForgotPassword.vue'
 import ResetPassword from '../views/ResetPasword.vue'
-import EmailVerification from '../views/EmailVerification.vue'
+import firebase from 'firebase'
+import "firebase/storage"
 import UserManagement from '../views/UserManagement.vue'
+
 
 Vue.use(VueRouter)
 
 const routes = [
   {
-    path: '/',
+    path: '/home',
     name: 'Home',
-    component: Home
+    component: Home,
+    meta: { requiresAuth: true }
+  },
+  {
+    path: '/edit',
+    name: 'Edit',
+    component: Edit,
+    meta: { requiresAuth: true }
+  },
+  {
+    path: '/settings',
+    name: 'Settings',
+    component: Settings,
+    meta: { requiresAuth: true }
   },
   {
     path: '/signup',
@@ -37,6 +54,22 @@ const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes
+})
+
+router.beforeEach((to, from, next) => {
+  if (to.matched.some(record => record.meta.requiresAuth)) {
+    // Load user
+    firebase.auth().onAuthStateChanged(user => {
+      // If user obj does not exist --> redirect to login page
+      if (!user) {
+        next({ name: 'Login' });
+      } else {
+        next();
+      }
+    });
+  } else {
+    next();
+  }
 })
 
 export default router
