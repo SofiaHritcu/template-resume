b25c68637f716f6f9a413d9d5482e225b2c988cf
StefanFrs <61791695+StefanFrs@users.noreply.github.com>
2021-08-25 18:04:51 +0300

    User cards added

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,21 @@
   <v-app>
     <router-view to="/login">Login</router-view>
     <Home />
+    <Users />
+    <router-view to="/users">Login</router-view>
   </v-app>
 </template>
 
 <script>
 import Login from './views/Login.vue';
-import Home from './views/Home.vue'
+import Home from './views/Home.vue';
+import Users from './views/Users.vue';
 
 export default {
   components: { 
   Login,
-  Home },
+  Home,
+  Users  },
   name: 'App',
   data: () => ({
     //
---
+++ src/views/Users.vue
@@ -0,0 +1,17 @@
+<template>
+<div>
+    <UsersCards position="Front end developer" card_image="profile.jpg" card_name="Jon doe"/>
+</div>
+</template>
+
+<script>
+import UsersCards from '../../src/components/users/usersCards.vue'
+
+export default {
+    name: 'Users',
+
+    components: {
+        UsersCards,
+    }
+}
+</script>
