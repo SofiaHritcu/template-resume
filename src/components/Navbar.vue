608b343c46cc7a2ab030b8683d35ff78c8c3e35f
SofiaHritcu <61913547+SofiaHritcu@users.noreply.github.com>
2021-08-26 15:53:24 +0300

    #template-portfolio - fix: bug regarding responsiveness inexistent after merge

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,22 +1,76 @@
-<template>
+ <template>
   <body>
-    <div class="sidebar">
-      <h1 class="text-h5 font-weight-thin" v-if="isLongName"> {{ name }} </h1>
-      <h1 class="text-h3 font-weight-thin" v-else> {{ name }} </h1>
+    <div class="sidebar" id="sidebar">
+      <div class="buttonss">
+        <button
+          type="button"
+          class="hamburger"
+          onclick="document.getElementById('sidebar').style.height = '100%'; document.getElementById('hamburger').style.display = 'none'; document.getElementById('xi').style.display = 'block';"
+          id="hamburger"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="50"
+            height="auto"
+            fill="currentColor"
+            class="bi bi-list"
+            viewBox="0 0 16 16"
+            color="white"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"
+            ></path>
+          </svg>
+        </button>
+        <button
+          type="button"
+          class="x"
+          id="xi"
+          onclick="document.getElementById('sidebar').style.height = '70px'; document.getElementById('hamburger').style.display = 'block'; document.getElementById('xi').style.display = 'none';"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="35"
+            height="auto"
+            fill="currentColor"
+            class="bi bi-x-lg"
+            viewBox="0 0 16 16"
+            color="white"
+          >
+            <path
+              d="M1.293 1.293a1 1 0 0 1 1.414 0L8 6.586l5.293-5.293a1 1 0 1 1 1.414 1.414L9.414 8l5.293 5.293a1 1 0 0 1-1.414 1.414L8 9.414l-5.293 5.293a1 1 0 0 1-1.414-1.414L6.586 8 1.293 2.707a1 1 0 0 1 0-1.414z"
+            ></path>
+          </svg>
+        </button>
+      </div>
+
+      <h1 class="text-h5 font-weight-thin" v-if="isLongName">{{ name }}</h1>
+      <h1 class="text-h3 font-weight-thin" v-else>{{ name }}</h1>
       <v-avatar size="250">
-        <v-img v-bind:src="profileImage"
-          max-height="200"
-          max-width="200">
+        <v-img v-bind:src="profileImage" max-height="200" max-width="200">
         </v-img>
       </v-avatar>
       <p class="description text-h6 font-weight-thin">{{ description }}</p>
       <div class="socials">
-        <a :href="`${ linkedInLink }`" class="fa fa-linkedin d-inline" v-if="linkedInLink"></a>
-        <a :href="`${ gitHubLink }`" class="fa fa-github d-inline" v-if="gitHubLink"></a>
-        <a :href="`${ instagramLink }`" class="fa fa-instagram d-inline" v-if="instagramLink"></a>
+        <a
+          :href="`${linkedInLink}`"
+          class="fa fa-linkedin d-inline"
+          v-if="linkedInLink"
+        ></a>
+        <a
+          :href="`${gitHubLink}`"
+          class="fa fa-github d-inline"
+          v-if="gitHubLink"
+        ></a>
+        <a
+          :href="`${instagramLink}`"
+          class="fa fa-instagram d-inline"
+          v-if="instagramLink"
+        ></a>
       </div>
       <hr />
-      <div class="sections">
+     <div class="sections">
         <div class="linkss d-flex">
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -33,7 +87,15 @@
               d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"
             />
           </svg>
-          <a href="/home" :class="[onAbout ? 'activeClass' : '','section-link', 'text-heading-5', 'font-weight-light', 'my-2']"
+          <a
+            href="/home"
+            :class="[
+              onAbout ? 'activeClass' : '',
+              'section-link',
+              'text-heading-5',
+              'font-weight-light',
+              'my-2',
+            ]"
             >About Me</a
           >
         </div>
@@ -54,11 +116,19 @@
               d="M1 6v-.5a.5.5 0 0 1 1 0V6h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1H1zm0 3v-.5a.5.5 0 0 1 1 0V9h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1H1zm0 2.5v.5H.5a.5.5 0 0 0 0 1h2a.5.5 0 0 0 0-1H2v-.5a.5.5 0 0 0-1 0z"
             />
           </svg>
-          <a href="/portfolio" :class="[onPortfolio ? 'activeClass' : '','section-link', 'text-heading-5', 'font-weight-light', 'my-2']"
+          <a
+            href="/portfolio"
+            :class="[
+              onPortfolio ? 'activeClass' : '',
+              'section-link',
+              'text-heading-5',
+              'font-weight-light',
+              'my-2',
+            ]"
             >Portfolio</a
           >
         </div>
-        <div class="linkss d-flex">
+         <div class="linkss d-flex">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             width="25"
@@ -75,7 +145,15 @@
               d="M5 8a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7A.5.5 0 0 1 5 8zm0-2.5a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5zm0 5a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5zm-1-5a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0zM4 8a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0zm0 2.5a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0z"
             />
           </svg>
-          <a href="/resume" :class="[onResume ? 'activeClass' : '','section-link', 'text-heading-5', 'font-weight-light', 'my-2']"
+          <a
+            href="/resume"
+            :class="[
+              onResume ? 'activeClass' : '',
+              'section-link',
+              'text-heading-5',
+              'font-weight-light',
+              'my-2',
+            ]"
             >Resume</a
           >
         </div>
@@ -96,7 +174,15 @@
               d="m2.165 15.803.02-.004c1.83-.363 2.948-.842 3.468-1.105A9.06 9.06 0 0 0 8 15c4.418 0 8-3.134 8-7s-3.582-7-8-7-8 3.134-8 7c0 1.76.743 3.37 1.97 4.6a10.437 10.437 0 0 1-.524 2.318l-.003.011a10.722 10.722 0 0 1-.244.637c-.079.186.074.394.273.362a21.673 21.673 0 0 0 .693-.125zm.8-3.108a1 1 0 0 0-.287-.801C1.618 10.83 1 9.468 1 8c0-3.192 3.004-6 7-6s7 2.808 7 6c0 3.193-3.004 6-7 6a8.06 8.06 0 0 1-2.088-.272 1 1 0 0 0-.711.074c-.387.196-1.24.57-2.634.893a10.97 10.97 0 0 0 .398-2z"
             />
           </svg>
-          <a href="/contact" :class="[onContact ? 'activeClass' : '','section-link', 'text-heading-5', 'font-weight-light', 'my-2']"
+          <a
+            href="/contact"
+            :class="[
+              onContact ? 'activeClass' : '',
+              'section-link',
+              'text-heading-5',
+              'font-weight-light',
+              'my-2',
+            ]"
             >Contact</a
           >
         </div>
@@ -147,15 +233,15 @@
         </div>
         <div class="my-3 ml-8 d-flex">
           <v-btn
-              large
-              color="#85a3e0"
-              class="white--text classic-button"
-              @click="logOut"
+            large
+            color="#85a3e0"
+            class="white--text classic-button"
+            @click="logOut"
           >
-              <v-icon left color="white">
-                  mdi-account-arrow-right-outline
-              </v-icon>
-              Log out
+            <v-icon left color="white">
+              mdi-account-arrow-right-outline
+            </v-icon>
+            Log out
           </v-btn>
         </div>
       </v-row>
@@ -164,33 +250,30 @@
 </template>
 
 <script>
-// :href="name"
-// import image from "../assets/testImg.jpg";
 import image from "../assets/account.png";
-import firebase from 'firebase'
-import 'firebase/storage'
-import db from '@/firebase/firebaseInit.js'
-
+import firebase from "firebase";
+import "firebase/storage";
+import db from "@/firebase/firebaseInit.js";
 export default {
   props: ['onAbout', 'onPortfolio', 'onResume', 'onContact', 'onEditAbout', 'onEditPortfolio', 'onEditProfile'],
   data() {
     return {
-      userID:'',
-      firstName: 'some',
-      lastName:'dude',
-      isLongName:'',
-      name: '',
+      userID: "",
+      firstName: "some",
+      lastName: "dude",
+      isLongName: "",
+      name: "",
       profileImage: image,
       description:
         "Hi, my name is Some Dude and I'm a senior software engineer. Welcome to my personal website!",
-      linkedInLink: '',
-      gitHubLink: '',
-      instagramLink: '',
-    };
+      linkedInLink: "",
+      gitHubLink: "",
+      instagramLink: "",
+      };
   },
   computed: {
     isLoggedIn() {
-      firebase.auth().onAuthStateChanged(user => {
+      firebase.auth().onAuthStateChanged((user) => {
         if (!user) {
           return false;
         } else {
@@ -199,17 +282,16 @@ export default {
           this.fetchCurrentUserData();
           return true;
         }
-    });
-    },
+      });
+      },
   },
   methods: {
     longName() {
       console.log(this.name);
       console.log(this.name.length > 12);
-      if(this.name.length > 12){
+      if (this.name.length > 12) {
         this.isLongName = true;
-      }
-      else {
+      } else {
         this.isLongName = false;
       }
     },
@@ -224,6 +306,11 @@ export default {
     },
     logOut() {
       console.log("logging out");
+      firebase.auth().signOut().then(() => {
+        isLoggedIn();
+      }).catch((error) => {
+        // An error happened.
+      });
     },
     fetchCurrentUserData() {
       db.collection('users').doc(this.userID).get()
@@ -254,8 +341,6 @@ export default {
   }
 };
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 .sidebar {
   height: 100%;
@@ -273,9 +358,11 @@ export default {
 h1 {
   color: #adc2eb;
   font-size: 32px;
+  margin-top: 50px;
 }
 .description {
   padding-top: 15px;
+   padding-top: 15px;
   padding-left: 15px;
   padding-right: 15px;
   font-size: 18px;
@@ -296,7 +383,6 @@ hr {
   align-items: center;
   padding-bottom: 10px;
 }
-
 .sidebar a {
   padding-left: 15px;
   text-decoration: none;
@@ -308,34 +394,49 @@ hr {
   opacity: 0.7;
 }
 
-.classic-button:hover{
-    background-color:#3366cc !important;
-}
+.classic-button:hover {
+  background-color: #3366cc !important;
+  }
 
 .activeClass {
-    color:#85a3e0 !important;
-    font-weight: 400 !important;
-}
+  color: #85a3e0 !important;
+  font-weight: 400 !important;
+  }
 
-.activeButton, .activeButton:hover {
+.activeButton,
+.activeButton:hover {
   color: #3366cc !important;
-  background-color:#85a3e0 !important;
+  background-color: #85a3e0 !important;
 }
+.hamburger {
+  float: left;
+  display: none;
+}
+.x {
+  float: right;
+  display: none;
+  margin-right: 25px;
+  }
 
-@media only screen and (max-width: 768px) {
+  @media only screen and (max-width: 768px) {
   .sidebar {
     height: auto;
     width: 100vw;
     overflow-y: hidden;
-    position: fixed;
+    position: absolute;
+    z-index: 10;
+    height: 70px;
+    padding-top: 15px;
+    padding-bottom: 15px;
   }
+  .template {
+    margin-bottom: px;
+  }
+  .hamburger {
+    display: block;
+    }
 }
 ::-webkit-scrollbar {
-  width: 10px;
-}
-
-/* Track */
-::-webkit-scrollbar-track {
   background: #f1f1f1;
 }
 
@@ -344,6 +445,13 @@ hr {
   background: #2952a3;
 }
 .fa:hover {
+  opacity: 0.7;
+}
+.hamburger:hover {
+  opacity: 0.7;
+}
+
+.x:hover {
   opacity: 0.7;
 }
 </style>
