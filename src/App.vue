ead3dca3cc663ccaf14f336339414f6a9da4cc06
Geo3777 <87963537+Geo3777@users.noreply.github.com>
2021-08-26 18:24:54 +0300

    add-resume-feature

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,19 @@
 <template>
   <v-app>
-    <AddResume></AddResume>
+    <router-view to="/login">Login</router-view>
   </v-app>
 </template>
 
 <script>
-import AddResume from "./views/AddResume.vue";
-import Resume from "./views/Resume.vue";
+import Login from './views/Login.vue';
+import Home from './views/Home.vue';
+
+
 export default {
-  components: {
-    AddResume,
-    Resume,
-  },
-  name: "App",
+  components: { 
+  Login,
+  Home },
+  name: 'App',
   data: () => ({
     //
   }),
