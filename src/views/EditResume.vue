23914e40f79ddf4599604cd490be9ba0d1235b79
SofiaHritcu <61913547+SofiaHritcu@users.noreply.github.com>
2021-08-27 00:32:20 +0300

    #template-portfolio - add: resume adding and resume page functionality done

--- src/views/EditResume.vue
+++ src/views/EditResume.vue
@@ -22,11 +22,23 @@
           </v-row> 
           <p class="text-h4 font-weight-thin resume-section" align="start">RESUME</p>
           <v-divider class="resume-divider my-10"></v-divider>
-          <form id="formmm" @submit="Submitt" ref="formm">
+          <v-snackbar
+            v-model="snackbarResumeUpdated"
+            :timeout="3000"
+            top
+            color="#85a3e0"
+          >
+            <span>Marvelous! You've updated your resume! It's looking great</span>
+            <v-btn color="white" text @click="snackbarResumeUpdated = false"
+              >Close</v-btn
+            >
+          </v-snackbar>
+          <form id="formmm" ref="formm">
             <div class="selectPDF">
               <v-file-input
                 truncate-length="50"
                 type="file"
+                v-model="resumeChosen"
                 id="image"
                 class="inputt my-10"
                 placeholder="Add your PDF file here"
@@ -40,30 +52,36 @@
             <div class="resume">
               <pdf v-bind:src="pdfFile" class="pdff mr-0"></pdf>
             </div>
-            <v-btn
-              class="btnn mx-2"
-              fab
-              dark
-              large
-              color="#3366cc"
-              type="submit"
-              form="formmm"
-            >
-              <v-icon dark>
-                mdi-pencil
+            <v-row style="width: 50%; margin-left: 26%" class="my-10">
+              <v-btn
+              color="#85a3e0"
+              class="white--text classic-button"
+              @click="submitUpdateResume"
+              >
+              <v-icon left color="white">
+                  mdi-chevron-triple-left
+              </v-icon>
+              Update RESUME
+              <v-icon right color="white">
+                  mdi-chevron-triple-right
               </v-icon>
             </v-btn>
+            </v-row>
           </form>
         </v-card>
       </v-container>
     </v-col>
+   
   </v-row>
+  
 </template>
 
 <script>
 import Navbar from "../components/Navbar.vue";
 
 import pdf from "vue-pdf";
+ import firebase from "firebase";
+import db from "@/firebase/firebaseInit";
 
 export default {
   components: {
@@ -72,7 +90,12 @@ export default {
   },
   data() {
     return {
+      userID: "",
       pdfFile: "",
+      resumeChosen: null,
+
+      //snackbar
+      snackbarResumeUpdated: false
     };
   },
   methods: {
@@ -93,17 +116,34 @@ export default {
       }
     },
 
-    Submitt() {
-      /*
-      db.collection("users")
-        .doc(idd)
-        .set({
-         
-        });
-    */
+    submitUpdateResume() {
+      console.log('Submitting new resume');
+      firebase.storage().ref(`resumes/${this.userID}.pdf`).put(this.resumeChosen);
+      this.snackbarResumeUpdated = true;
       this.$refs.formm.reset();
+      this.$router.push('resume'); 
     },
   },
+  mounted() {
+    firebase.auth().onAuthStateChanged((user) => {
+      if (!user) {
+        return false;
+      } else {
+        this.userID = user.uid;
+        firebase
+          .storage()
+          .ref(`resumes/${this.userID}.pdf`)
+          .getDownloadURL()
+          .then((url) => {
+            this.pdfFile = url;
+          });
+        return true;
+      }
+    });
+  },
+
+  
+
 };
 </script>
 
