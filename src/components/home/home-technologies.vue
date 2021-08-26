4b3577cb16c4989fbf571ce703af55aa3fe20653
Cosmin Jescu <57216949+jesq@users.noreply.github.com>
2021-08-26 19:46:09 +0300

    Added: Fetch portfolio data #1

--- src/components/home/home-technologies.vue
+++ src/components/home/home-technologies.vue
@@ -3,14 +3,6 @@
 <!-- technologies section (home) -->
 <div class="overview-section p-3 p-lg-5">
     <div class="container">
-        <div class="what-i-do pl-5 mb-4">
-            <h3>What I do </h3>
-        </div>
-
-        <div class="skills_description pb-5">
-            <p>{{skills_description}}</p>
-        </div>
-
         <!-- Skills with image section -->
         <div class="row pt-5">
             <div class="pt-5 skill-cols col-lg-4">
@@ -38,23 +30,6 @@
                 </div>
             </div>
         </div>
-
-        <!-- Skills without image section -->
-
-        <div class="row pt-5">
-            <div class="technology-col col-xl-4">
-                <h4>{{skill_title}}</h4>
-                <p>{{skill_details}}</p>
-            </div>
-            <div class="technology-col col-xl-4">
-                <h4>{{skill_title}}</h4>
-                <p>{{skill_details}}</p>
-            </div>
-            <div class="technology-col col-xl-4">
-                <h4>{{skill_title}}</h4>
-                <p>{{skill_details}}</p>
-            </div>
-        </div>
     </div>
 </div>
 </template>
@@ -64,7 +39,6 @@ export default {
     name: 'Home_technologies',
 
     props: {
-        skills_description: String,
         skill_title: String,
         skill_details: String,
         skill_image:String,
@@ -74,27 +48,6 @@ export default {
 </script>
 
 <style scoped>
-.what-i-do {
-    font-size: 2rem;
-    position: relative;
-    padding-left: 1.5rem;
-}
-
-.what-i-do h3 {
-    font-size: 2rem;
-}
-
-.what-i-do:before {
-    content: "";
-    display: inline-block;
-    width: 5px;
-    height: 100%;
-    background: #3366cc;
-    position: absolute;
-    left: 0;
-    top: 0;
-}
-
 .image-skill img {
     width: 120px;
     height: 120px;
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,8 +4,8 @@
       <navbar :onAbout="true"></navbar>
     </v-col>
     <v-col cols="9">
-      <Home_introduction_section picture_user="profile.jpg" user_name="Simon Doe" job_name="Software developer" job_description="I'm a software engineer specialised in frontend and backend development for complex scalable web apps. I write about software development on my blog. Want to know how I may help your project? Check out my project portfolio and online resume." />
-      <Home_technologies skill_title_image_section="CSS" skill_image="css.png" skill_title="Vanilla JavaScript" skill_details="List skills/technologies here. You can change the icon above to any of the 1500+ FontAwesome 5 free icons available. Aenean commodo ligula eget dolor." skills_description="I have more than 10 years' experience building software for clients all over the world. Below is a quick overview of my main technical skill sets and technologies I use. Want to find out more about my experience? Check out my online resume and project portfolio." />
+      <Home_introduction_section :picture_user="pictureLink" :firstName="firstName"
+       :lastName="lastName" :job_name="role" :job_description="roleDescription" :skillsDescription="skillDescription"/>
       <Home_slider_projects project_name="Proiectul NO1" project_description="Cel mai smecher proiect ever"/>
       <Home_learning learning_name="CSS" learning_image="css.png"/>
     </v-col>
@@ -19,6 +19,8 @@ import Home_technologies from '../../src/components/home/home-technologies.vue'
 import Home_slider_projects from '../../src/components/home/home-slider-projects'
 import Home_learning from '../../src/components/home/home-learning.vue'
 import Navbar from '../components/Navbar.vue'
+import firebase from "firebase";
+import db from "@/firebase/firebaseInit";
 
   export default {
     name: 'Home',
@@ -29,6 +31,71 @@ import Navbar from '../components/Navbar.vue'
     Home_slider_projects,
     Home_learning,
         Navbar,
+    },
+
+    data() {
+      return {
+        userID: '',
+        firstName: '',
+        lastName: '',
+        pictureLink: '',
+        role: '',
+        roleDescription: '',
+        skillDescription: '',
+      }
+    },
+
+    created() {
+      this.getAllData();
+    },
+
+    methods: {
+      loginRedirect() {
+        this.$router.push("/login");
+      },
+
+      async getAllData() {   
+        await this.getUserID();
+        await this.fetchUserData();
+        this.fetchPortofolioData();
+      },
+
+     getUserID() { //trebuie modificata putin functia asta!!
+       firebase.auth().onAuthStateChanged(user => {
+         this.userID = user.uid;
+       })  
+      },
+
+      async fetchUserData() {
+        var docRef = db.collection('users').doc(this.userID);
+        docRef.get().then((doc) => {
+          if(doc.exists) {
+            this.firstName = doc.data().firstName;
+            this.lastName = doc.data().lastName;
+            var storageRef = firebase.storage().ref(`profilePictures/${this.userID}.jpg`).getDownloadURL()
+            .then((url)=> {
+              this.pictureLink = url;
+          })
+          } else {
+            console.log("No such document")
+            this.loginRedirect();
+          }
+        }).catch((error) => {
+          console.log("Error getting document: ", error)
+          this.loginRedirect();
+        });
+      },
+      fetchPortofolioData() {
+        var docRef = db.collection('portofolios').doc(this.userID);
+        docRef.get().then((doc) => {
+          if(doc.exists) {
+            this.role = doc.data().role;
+            this.roleDescription = doc.data().roleDescription;
+            this.skillDescription = doc.data().skillDescription;
+          }
+        })
+      }
     }
+    
   }
 </script>
