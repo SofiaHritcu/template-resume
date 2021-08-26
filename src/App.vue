648154eaba747a8d8eac6add518fcc48b1a05634
StefanFrs <61791695+StefanFrs@users.noreply.github.com>
2021-08-26 11:12:09 +0300

    prepare to push in develop

--- src/App.vue
+++ src/App.vue
@@ -2,19 +2,17 @@
   <v-app>
     <router-view to="/login">Login</router-view>
     <Home />
-    <Projects/>
   </v-app>
 </template>
 
 <script>
 import Login from './views/Login.vue';
 import Home from './views/Home.vue';
-import Projects from './views/Projects';
+
 
 export default {
   components: { 
   Login,
-  Projects,
   Home },
   name: 'App',
   data: () => ({
