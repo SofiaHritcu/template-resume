9711b97f5e20702485a3f5f3140f51d05ec1a5cc
StefanFrs <61791695+StefanFrs@users.noreply.github.com>
2021-08-26 11:13:17 +0300

    Feature/#31 projects page (#13)
    * project-cards added
    
    * cards added (without dynamic image)
    
    * card-background-image
    
    * prepare to push in develop
    
    Co-authored-by: Geo3777 <87963537+Geo3777@users.noreply.github.com>

---
+++ src/components/projects/introduction-project-section.vue
@@ -0,0 +1,55 @@
+<template>
+<!-- contact introduction -->
+<div class="about-me-section p-3 p-lg-5">
+    <div class="container text-center contact-intro ">
+        <div class="text-section pb-md-0 pb-4">
+            <h3>Projects</h3>
+            <p class="pt-3">{{description_projects}} </p>
+        </div>
+    </div>
+</div>
+</template>
+
+<script>
+export default {
+    name: 'Introduction_project_section',
+
+    props: {
+        description_projects: String,
+
+    }
+}
+</script>
+
+<style scoped>
+p,
+h2,
+h3,
+h4,
+h5 {
+    /* font color */
+    color: #292929;
+}
+
+h2 {
+    font-size: 3rem;
+}
+
+h3 {
+    font-size: 2rem;
+    font-weight:bold;
+}
+
+p {
+    font-size: 1rem;
+}
+
+.about-me-section {
+    background: #fafafa;
+}
+
+.contact-intro {
+    max-width: 780px;
+}
+
+</style>
---
+++ src/views/Projects.vue
@@ -0,0 +1,21 @@
+<template>
+ <div>
+     <Introduction_project_section description_projects="Here you can check all of my projects."/>
+     <Cards_project_section background="profile.jpg" project_name="Proiectul no1" project_description="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius esse corporis, velit porro impedit laudantium accusamus! Id velit, illum magni rem mollitia blanditiis iste maiores optio ipsa, est dolorem fugit. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius esse corporis, velit porro impedit laudantium accusamus! Id velit, illum magni rem mollitia blanditiis iste maiores optio ipsa, est dolorem fugit."/>
+ </div>
+</template>
+
+<script>
+
+import Introduction_project_section from '../../src/components/projects/introduction-project-section.vue'
+import Cards_project_section from '../../src/components/projects/cards-project-section.vue'
+
+  export default {
+    name: 'Projects',
+
+    components: {
+    Introduction_project_section,
+    Cards_project_section
+    }
+  }
+</script>
