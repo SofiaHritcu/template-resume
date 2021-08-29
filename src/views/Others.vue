e2ba273d18ac145dce65f647f116769d4dd3b742
SofiaHritcu <61913547+SofiaHritcu@users.noreply.github.com>
2021-08-29 18:46:30 +0300

    #template-portfolio - update: load card users on others page

--- src/views/Others.vue
+++ src/views/Others.vue
@@ -1,56 +1,74 @@
 <template>
-<v-row>
-  <v-col cols="2">
-    <navbar></navbar>
-  </v-col>
-  <v-col cols="10">
-  </v-col>
-</v-row>
+  <v-row>
+    <v-col cols="2">
+      <navbar></navbar>
+    </v-col>
+    <v-col cols="10">
+      <users-introduction></users-introduction>
+      <div class="container our-services">
+        <v-row align="center" justify="center" class="ml-13">
+          <users-cards
+            v-for="user in users"
+            :role="user.role"
+            :image="user.image"
+            :name="user.firstName + ' ' + user.lastName"
+          />
+        </v-row>
+      </div>
+    </v-col>
+  </v-row>
 </template>
 
 <script>
-
-import Navbar from '../components/Navbar.vue'
+import Navbar from "../components/Navbar.vue";
 import firebase from "firebase";
 import db from "@/firebase/firebaseInit";
+import UsersIntroduction from "../components/users/usersIntroduction.vue";
+import UsersCards from "../components/users/usersCards.vue";
 
-  export default {
-    name: 'Others',
+export default {
+  name: "Others",
 
-    data() {
-        return {
-            users: [],
-        };
-    },
+  data() {
+    return {
+      users: [],
+    };
+  },
 
-    components: {
-        Navbar
-    },
-    mounted() {
-        db.collection("users").get().then(async (usersFetched) => {
-            usersFetched.forEach( async (userFetched) => {
-                var firstName = userFetched.data().firstName;
-                var lastName = userFetched.data().lastName;
-                var uid = userFetched.id;
-                var profileImage = await  firebase
-                                    .storage()
-                                    .ref(`profilePictures/${uid}.jpg`)
-                                    .getDownloadURL();
-                var portfolioFetched = await db.collection("portofolios").doc(uid).get();
-                var role = '';
-                if (portfolioFetched.data() !== undefined) {
-                    role = portfolioFetched.data().role;
-                };
-                this.users.push({ 
-                    firstName: firstName, 
-                    lastName: lastName, 
-                    role: role, 
-                    uid: uid, 
-                    image: profileImage
-                    }, 
-                );
-            });
+  components: {
+    Navbar,
+    UsersIntroduction,
+    UsersCards,
+  },
+  mounted() {
+    db.collection("users")
+      .get()
+      .then(async (usersFetched) => {
+        usersFetched.forEach(async (userFetched) => {
+          var firstName = userFetched.data().firstName;
+          var lastName = userFetched.data().lastName;
+          var uid = userFetched.id;
+          var profileImage = await firebase
+            .storage()
+            .ref(`profilePictures/${uid}.jpg`)
+            .getDownloadURL();
+          var portfolioFetched = await db
+            .collection("portofolios")
+            .doc(uid)
+            .get();
+          var role = "";
+          if (portfolioFetched.data() !== undefined) {
+            role = portfolioFetched.data().role;
+          }
+          this.users.push({
+            firstName: firstName,
+            lastName: lastName,
+            role: role,
+            uid: uid,
+            image: profileImage,
+          });
         });
-    }
-  }
+      });
+  },
+};
 </script>
